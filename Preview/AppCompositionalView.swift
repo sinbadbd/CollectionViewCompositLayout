4d09d8e9279240751a5b696588f0682507b8781f
Imran <imufun2@gmail.com>
2021-01-26 22:20:27 +0600

    composition layout init (#4)

--- Preview/AppCompositionalView.swift
+++ Preview/AppCompositionalView.swift
@@ -6,16 +6,57 @@
 //
 
 import SwiftUI
+//import UIKit
+
+class CompositionalController : UICollectionViewController {
+    
+    
+    init() {
+        let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+        item.contentInsets.bottom = 16
+        item.contentInsets.trailing = 16
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(0.8), heightDimension: .absolute(300)), subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .groupPaging
+        section.contentInsets.leading = 32
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        super.init(collectionViewLayout: layout)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        collectionView.backgroundColor = .white
+        navigationItem.title = "Apps"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    
+}
+
 
 
 struct AppView: UIViewControllerRepresentable {
     typealias UIViewControllerType = UIViewController
     
     func makeUIViewController(context: UIViewControllerRepresentableContext<AppView>) -> UIViewController {
-        let redController = UIViewController()
-        redController.view.backgroundColor = .red
-        
-        return redController
+        let vc = CompositionalController()
+        return vc
     }
     
     func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<AppView>) {
@@ -28,11 +69,13 @@ struct AppView: UIViewControllerRepresentable {
 struct AppCompositionalView: View {
     var body: some View {
         AppView()
+        
     }
 }
 
 struct AppView_Previews: PreviewProvider {
     static var previews: some View {
         AppCompositionalView()
+            .edgesIgnoringSafeArea(.all)
     }
 }
