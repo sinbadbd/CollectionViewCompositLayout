5fed59f6a92b0e11c02ac02c9e005d203f00db2b
imran <imufun2@gmail.com>
2021-01-27 11:02:20 +0600

    multiple cell add

--- Preview/AppCompositionalView.swift
+++ Preview/AppCompositionalView.swift
@@ -12,6 +12,33 @@ class CompositionalController : UICollectionViewController {
     
     
     init() {
+        
+        let layout = UICollectionViewCompositionalLayout {
+            (sectionNumber, _) -> NSCollectionLayoutSection? in
+       
+            if sectionNumber == 0 {
+                return CompositionalController.topSection()
+            }else if sectionNumber == 1{
+                let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/3)))
+                item.contentInsets = .init(top: 0, leading: 0, bottom: 16, trailing: 16)
+                
+                let group = NSCollectionLayoutGroup.vertical(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(300)), subitems: [item])
+                let section = NSCollectionLayoutSection(group: group)
+                
+                section.orthogonalScrollingBehavior = .groupPaging
+                section.contentInsets.leading = 16
+                return section
+            }else {
+                return CompositionalController.topSection()
+            }
+        }
+        
+        super.init(collectionViewLayout: layout)
+ 
+    }
+    
+    static func topSection()-> NSCollectionLayoutSection {
+        
         let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
         item.contentInsets.bottom = 16
         item.contentInsets.trailing = 16
@@ -19,10 +46,8 @@ class CompositionalController : UICollectionViewController {
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(0.8), heightDimension: .absolute(170)), subitems: [item])
         let section = NSCollectionLayoutSection(group: group)
         section.orthogonalScrollingBehavior = .groupPaging
-        section.contentInsets.leading = 32
-        let layout = UICollectionViewCompositionalLayout(section: section)
-        
-        super.init(collectionViewLayout: layout)
+        section.contentInsets.leading = 16
+        return section
     }
     
     required init?(coder: NSCoder) {
@@ -38,16 +63,34 @@ class CompositionalController : UICollectionViewController {
     }
     
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 5
+        return 3
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
     }
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .red
-        return cell
+      
+        switch indexPath.section {
+        case 0:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+            cell.backgroundColor = .red
+            return cell
+        
+        case 1:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+            cell.backgroundColor = .blue
+            return cell
+            
+        default:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+            cell.backgroundColor = .green
+            return cell
+ 
+            
+        
+        }
+        
     }
     
     
